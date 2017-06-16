bf2d93b38cebc3fa699469710da049785dff34d2
Yann Carlen <yann.carlen@epitech.eu>
2017-06-17 00:28:58 +0200

    add argv parsing

--- app.js
+++ app.js
@@ -6,8 +6,28 @@ const wesh = console.log
 
 const exit = (code=0) => process.exit(code)
 
+const args = argv.option([
+    {
+        name: 'name',
+        type: 'string',
+        description: 'Name of the mates',
+        example: "./app --name='Issou CPP'"
+    },
+       {
+        name: 'room',
+        type: 'string',
+        description: 'Name of the room',
+        example: "./app --room='sm1|sm2|sm3|...'"
+    }
+]).run()
+
+if (args.options.name === undefined || args.options.room === undefined) {
+    argv.help()
+    exit()
+}
+
 io.on('connect', () => {
-    //io.emit('join', { id: 1 })
+    io.emit('join', { name: args.options.name, room: args.options.room })
     wesh('I\' am connected')
 })
 
