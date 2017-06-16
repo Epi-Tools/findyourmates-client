36ed527f653d28cee958182406ae0fb033d04340
Yann Carlen <yann.carlen@epitech.eu>
2017-06-16 23:34:08 +0200

    add basic socket

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const argv = require('argv')
+const socket = require('socket.io-client')
+const io = socket.connect('http://127.0.0.1:3000')
+
+const wesh = console.log
+
+const exit = (code=0) => process.exit(code)
+
+io.on('connect', () => {
+    //io.emit('join', { id: 1 })
+    wesh('I\' am connected')
+})
+
+io.on('disconnect', () => {
+    wesh('Disconnected')
+    exit()
+})
